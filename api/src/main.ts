e13f632cf80d37643d8bd376b500f8a3a5f0795f
Anna sganderla <sganderlakarlaanna@gmail.com>
2023-10-28 23:35:26 -0300

    fix - liberando o cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -24,7 +24,7 @@ async function bootstrap() {
     SwaggerModule.setup('docs', app, document);
   }
 
-  app.enableCors({ origin: 'http://localhost:3000' });
+  app.enableCors({ origin: '*' });
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
