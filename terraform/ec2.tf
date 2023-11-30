564de2df7a6ace33a446f5ceade73a544612d575
VitorDanielAdams <vitor.adams@outlook.com>
2023-11-30 19:51:08 -0300

    Fix - ami fix

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "avt" {
-  ami           = "ami-751212661018"
+  ami           = "ami-07d131ac12352754d"
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.eks_subnet1.id
 }
