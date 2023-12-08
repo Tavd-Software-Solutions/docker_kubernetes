2b793361ccd2a007bd33b0218c05a0822c5e1109
VitorDanielAdams <vitor.adams@outlook.com>
2023-12-07 22:31:04 -0300

    Fix - teste5

--- main.tf
+++ main.tf
@@ -1,5 +1,8 @@
 provider "aws" {
-  region = "us-east-1"
+  region     = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
+  token      = var.token
 }
 
 resource "aws_vpc" "eks_vpc" {
--- variables.tf
+++ variables.tf
@@ -1,3 +1,9 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
 variable "vpc_block" {
   description = "CIDR block for the VPC"
   type        = string
@@ -20,4 +26,19 @@ variable "vpc_name" {
   description = "Name tag for the VPC"
   type        = string
   default     = "vpc"
+}
+
+variable "access_key" {
+  type    = string
+  default = "ASIAQZMHI4CLNIEWEPUX"
+}
+
+variable "secret_key" {
+  type    = string
+  default = "YjFcSthptv8Q4699FOGDclEBANVkou2+y0Fyl8pX"
+}
+
+variable "token" {
+  type    = string
+  default = "FwoGZXIvYXdzEFkaDM4WnPnfZZCMhVQrQyLDAfxAYFgNDRZOiKRLaDYGJ2A7yvEbf4Ku7uSv6Gm7+cSQCL1ptHPI9fA9fGyjeIGBOB/yNN8bBIYYU1gFWek/Lit3XTdchMYSHJSSHRjRCf2jJr9r+sUL89bxBWJUfUub3t2eY6wsIxayLjiTdjq8l20ceceTdlp3lmrTzRmK9Il9QPB9zzVy1vMESZPff2ABRyfrPOakvv7zi+BHTj4cCq6dOvUfhlgadv482fjgPGl1DYGEwUJ7n88yEkWTxAByZUk0myj8pcmrBjItTrMdHcRE7lk1RbHX0g4woywdI6KC9ykyT+2B4tVERaoxeqS+RQAuPSD1s294"
 }
