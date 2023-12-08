1febbd79491223bb623aec3f0afa5af0aed021a9
VitorDanielAdams <vitor.adams@outlook.com>
2023-12-08 00:06:46 -0300

    Fix - inventario

--- main.tf
+++ main.tf
@@ -39,25 +39,9 @@ resource "aws_instance" "ec2_instance" {
   tags = {
     Name = "EC2-Instance"
   }
-
-  vpc_security_group_ids = [aws_security_group.ua-sg.id]
-  subnet_id = aws_subnet.eks_subnet1.id
 }
 
 output "public_ip" {
   value = aws_instance.ec2_instance.public_ip
 }
 
-resource "aws_security_group" "ua-sg" {
-  name        = "ua-sg"
-  description = "Security group for EC2 instance"
-  vpc_id = aws_vpc.eks_vpc.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-}
